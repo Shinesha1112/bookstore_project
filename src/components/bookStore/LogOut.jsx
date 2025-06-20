8794fe32d77cf1e911c9a7058b2d1914774bf5e6
Shinesha W <shinesha1112@gmail.com>
2025-06-20 22:50:33 +0530

    changes in logout button

--- src/components/bookStore/LogOut.jsx
+++ src/components/bookStore/LogOut.jsx
@@ -15,20 +15,6 @@ function LogOut() {
   <button
     onClick={handleLogOut}
   className='logOutButton'
-    style={{
-      position: 'absolute',
-      top: '20px',
-      right: '20px',
-      padding: '8px 16px',
-      backgroundColor: '#fff',
-      color: '#333',
-      border: 'none',
-      borderRadius: '6px',
-      boxShadow: '0 2px 8px rgba(0,0,0,0.2)',
-      cursor: 'pointer',
-      background:'#793d01',
-    color:'white'
-    }}
   >
     LogOut
   </button>
