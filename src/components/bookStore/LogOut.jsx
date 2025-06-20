58e2c7373579b85d34b5ee7fdaff49430a64742b
Shinesha W <shinesha1112@gmail.com>
2025-06-21 00:02:55 +0530

    changes in logoutbutton

--- src/components/bookStore/LogOut.jsx
+++ src/components/bookStore/LogOut.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import '../BookStore_css/LogOut.css';
 
 function LogOut() { 
   const navigate=useNavigate();
@@ -10,21 +11,9 @@ function LogOut() {
 
     }
   return (
-    <div style={{ position: 'relative' }}>
-  <button
+    <div className='logOutButton'style={{ position: 'relative' }}>
+  <button 
     onClick={handleLogOut}
-  style={{
-     position: 'absolute',
-    top: '20px',
-    right:'1px',
-    padding: '8px 16px',
-    backgroundColor:'#6b1a1a',
-    color: 'white',
-    border: 'none',
-    borderRadius: '6px',
-    boxShadow: '0 2px 8px rgba(0,0,0,0.2)',
-    cursor: 'pointer',
-  }}
   >
     LogOut
   </button>
